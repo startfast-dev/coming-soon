48ce16b76e9d977f0daf0846de7e33ca3cf3ea5c
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2024-08-30 19:14:25 +0600

    update the coming soon template

--- data/data.ts
+++ data/data.ts
@@ -2,13 +2,13 @@ const currentYear = new Date().getFullYear();
 const data = {
   sitename: "StartFast",
   sitetagline: "Something great is on the way! ✨ 🔥",
-  siteurl: "https://github.com/baikaresandip",
+  siteurl: "#",
   sitelogo: "",
   title: "Coming Soon!",
   description:
-    "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
   newsletterheading: "Stay tuned for our live updates!",
-  copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">StartFast</a>`,
+  copyrightText: `Copyright © ${currentYear} | Design and Developed By StartFast`,
   socialIconsHeading: "",
   hideSubscribeForm: false, // make true to disable subscription form
   socialIcons: [],
